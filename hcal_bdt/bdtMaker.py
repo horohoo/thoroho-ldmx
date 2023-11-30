80ee65e29604cad8902bd810fb1fe46f4084bf1e
Tyler Horoho <tgh7hx@virginia.edu>
2023-11-30 11:23:58 -0500

    Latest Hcal BDT

--- hcal_bdt/bdtMaker.py
+++ hcal_bdt/bdtMaker.py
@@ -73,21 +73,39 @@ class sampleContainer:
                     xmean=0
                     ymean=0
                     zmean=0
+                    rmean=0
+
+                    xmean_equal=0
+                    ymean_equal=0
+                    zmean_equal=0
+                    rmean_equal=0
+
+                    rms_r=0
+                    rms_z=0
 
                     xstd=0
                     ystd=0
                     zstd=0
+
+                    xstd_equal=0
+                    ystd_equal=0
+                    zstd_equal=0
                     
                     Etot = 0
-                    layershit = []
 
                     for it in SimParticles:
                         for sphit in TargetScoringPlaneHits:
                             if sphit.getPosition()[2] > 0:
                                 if it.first == sphit.getTrackID():
-                                    if sphit.getPdgID() == 11 and 0 in it.second.getParents():
-                                        x0_gamma = sphit.getPosition()
-                                        p_gamma = [-sphit.getMomentum()[0], -sphit.getMomentum()[1], 4000 - sphit.getMomentum()[2]]
+                                    if isBkg:
+                                        if sphit.getPdgID() == 11 and 0 in it.second.getParents():
+                                            x0_gamma = sphit.getPosition()
+                                            p_gamma = [-sphit.getMomentum()[0], -sphit.getMomentum()[1], 4000 - sphit.getMomentum()[2]]
+                                    else:
+                                        if sphit.getPdgID() == 622:
+                                            x0_gamma = sphit.getPosition()
+                                            p_gamma = sphit.getMomentum()
+
                     downstreamrmean_gammaproj = 0 
                     
                     
@@ -96,15 +114,23 @@ class sampleContainer:
                         x = hit.getXPos()
                         y = hit.getYPos()
                         z = hit.getZPos()
+                        r = math.sqrt(x*x + y*y)
+
                         energy = hit.getEnergy()
                         Etot += energy
-
-                        if not z in layershit:
-                            layershit.append(z)
                         
                         xmean += x*energy
                         ymean += y*energy
                         zmean += z*energy
+                        rmean += r*energy
+
+                        xmean_equal += x
+                        ymean_equal += y
+                        zmean_equal += z
+                        rmean_equal += r
+
+                        rms_r += r*r
+                        rms_z += z*z
 
                         x_proj = x0_gamma[0] + (z - x0_gamma[2])*p_gamma[0]/p_gamma[2]
                         y_proj = x0_gamma[1] + (z - x0_gamma[2])*p_gamma[1]/p_gamma[2]
@@ -129,6 +155,14 @@ class sampleContainer:
                     xmean /= Etot
                     ymean /= Etot
                     zmean /= Etot
+                    rmean /= Etot
+
+                    xmean_equal /= hits
+                    ymean_equal /= hits
+                    zmean_equal /= hits
+                    rmean_equal /= hits
+                    rms_r /= hits
+                    rms_z /= hits
                         
                     downstreamrmean_gammaproj /= Etot    
                      
@@ -142,28 +176,51 @@ class sampleContainer:
                         ystd += energy*(y-ymean)**2
                         zstd += energy*(z-zmean)**2
 
+                        xstd_equal += (x-xmean_equal)**2
+                        ystd_equal += (y-ymean_equal)**2
+                        zstd_equal += (z-zmean_equal)**2
+
+
                     xstd = math.sqrt(xstd/Etot)
                     ystd = math.sqrt(ystd/Etot)
                     zstd = math.sqrt(zstd/Etot)
+
+                    xstd_equal = math.sqrt(xstd_equal/hits)
+                    ystd_equal = math.sqrt(ystd_equal/hits)
+                    zstd_equal = math.sqrt(zstd_equal/hits)
+
+                    rms_r = math.sqrt(rms_r)
+                    rms_z = math.sqrt(rms_z)
     
                     # Fill event with features to train BDT
-                  
-                    evt.append(xstd) #0
-                    evt.append(ystd) #1
-                    evt.append(zstd) #2
-                               
-                    evt.append(xmean) #3
-                    evt.append(ymean) #4
-                    
-                    evt.append(isohits) #5
-                    evt.append(isoE) #6
-                    evt.append(hits) #7
-                    
-                    evt.append(Etot) #8
-                    
-                    evt.append(downstreamrmean_gammaproj) #9
 
-                    evt.append(len(layershit)) #10
+                    evt.append(rms_r) #0
+                    evt.append(rms_z) #1
+                  
+                    evt.append(xstd) #2
+                    evt.append(ystd) #3
+                    evt.append(zstd) #4
+                               
+                    evt.append(xmean) #5
+                    evt.append(ymean) #6
+                    evt.append(rmean) #7
+
+                    #evt.append(zstd_equal) #8
+                    #evt.append(xstd_equal) #9
+                    #evt.append(ystd_equal) #10
+
+                    #evt.append(rmean_equal) #11
+                    #evt.append(xmean_equal) #12
+                    #evt.append(ymean_equal) #13
+                    
+                    evt.append(isohits) #14
+                    evt.append(isoE) #15
+                    evt.append(hits) #16
+                    
+                    evt.append(Etot) #17
+                    
+                    evt.append(downstreamrmean_gammaproj) #18
+
 
                     self.events.append(evt)
                     evtcount += 1
@@ -182,8 +239,6 @@ class sampleContainer:
         print("Final Event Shape: ", np.shape(self.events))
 
     def constructTrainAndTest(self):
-        
-        
         
         self.train_x = self.events[0:int(len(self.events)*self.trainFrac)]
         self.test_x = self.events[int(len(self.events)*self.trainFrac):]
@@ -216,16 +271,17 @@ if __name__ == '__main__':
     parser = OptionParser()
 
     parser.add_option('--seed', dest='seed', type='int', default=4, help='Numpy random seed.')
-    parser.add_option('--train_frac', dest='train_frac', default=0.80, help='Fraction of events to use for training')
+    parser.add_option('--train_frac', dest='train_frac', default=0.001, help='Fraction of events to use for training')
     parser.add_option('--max_evt', dest='max_evt', type='int', default=500000, help='Max Events to load')
     parser.add_option('--out_name', dest='out_name', default='bdt', help='Output Pickle Name')
+    parser.add_option('--bdt_path', dest='bdt_path', default='/sfs/qumulo/qhome/tgh7hx/ldmx/hcal_bdt_weights_v2.pkl', help='BDT model to load in')
     
     
     parser.add_option('--swdir', dest='swdir', default='/sfs/qumulo/qhome/lbc2qnt/ldmx-sw/install', help='ldmx-sw build directory')
     parser.add_option('--eta', dest='eta', type='float', default=0.023, help='Learning Rate')
     parser.add_option('--tree_number', dest='tree_number', type='int', default=1000, help='Tree Number')
     parser.add_option('--depth', dest='depth', type='int', default=6, help='Max Tree Depth')
-    parser.add_option('--bkg_dir', dest='bkg_dir', default='/project/hep_aag/ldmx/background/PN_Ecal/sim/', help='name of background file directory')
+    parser.add_option('--bkg_dir', dest='bkg_dir', default='/scratch/tgh7hx/v3.2.0_ecalPN_tskim-batch1/', help='name of background file directory')
     parser.add_option('--sig_dir', dest='sig_dir', default='/project/hep_aag/ldmx/ap/visibles/produced/mAp_050/', help='name of signal file directory')
 
 
@@ -279,8 +335,8 @@ if __name__ == '__main__':
 
     num_trees = options.tree_number
     evallist = [(eventContainer.dtest,'eval'), (eventContainer.dtrain,'train')]
-    gbm = xgb.train(params, eventContainer.dtrain, num_trees, evallist)
-
+    #gbm = xgb.train(params, eventContainer.dtrain, num_trees, evallist)
+    gbm = pkl.load(open(options.bdt_path, 'rb')) # use this option for testing on a previously trained bdt
 
     preds = gbm.predict(eventContainer.dtest)
     fpr, tpr, threshold = metrics.roc_curve(eventContainer.test_y, preds)
